4abe01140530d7c934ac252b128415aff6bb7836
Amanuel <amanuel.habtamu@aastustudent.edu.et>
2026-02-11 11:18:06 +0300

    Leetcode solution for '3438. Find Valid Pair of Adjacent Digits in String'

---
+++ 3438. Find Valid Pair of Adjacent Digits in String.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findValidPair(self, s: str) -> str:
+        count = Counter(s)
+
+        for i in range(len(s) - 1):
+            
+            if int(s[i]) != int(s[i + 1]) and int(s[i]) == count[s[i]] and int(s[i + 1]) == count[s[i + 1]]:
+                return s[i:i + 2]
+        return ""
